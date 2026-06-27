f4e250b41a9fe6ccc7461dd930f49972010b7573
DENNIS JOBBY <155895741+dennisjobs@users.noreply.github.com>
2026-06-27 14:09:05 +0530

    Delete if_neg.m

--- if_neg.m
+++
@@ -1,7 +0,0 @@
-function flag = if_neg(G)
-%IF_NEG Returns true if all elements of G are non-negative
-%   Input:  G - any matrix
-%   Output: flag - logical true (1) if G ≥ 0 element-wise, else false (0)
-
-flag = all(G(:) >= 0);
-end
